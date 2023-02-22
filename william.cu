ff0973f875a98724ea4481a6f144d783fdfbaac8
Wayzm <will@pop-os.localdomain>
2023-02-22 20:20:14 +0100

    sym

--- william.cu
+++ william.cu
@@ -41,8 +41,25 @@ __global__ void grey_img(ui32* dr, ui32* dg, ui32* db, ui32 width, ui32 height){
     db[idx] = value;
 }
 
+/*** DO NOT FORGET TO GREY SCALE FIRST***/
 __global__ void sobel(){
+    ui32 value[3][3];
+    // Compute thread id
+    const ui32 x = threadIdx.x + blockDim.x * blockIdx.x;
+    const ui32 y = threadIdx.y + blockDim.y * blockIdx.y;
+    // const ui32 idx = y * (width) + x;
+    /*** SOBEL ALGORITHM ***/
+    /**  Sobel matrix x threads of each block
+    ** | -1 0 1 |   | (0 0) (0 1) (0 2) |
+    ** | -2 0 2 | x | (1 0) (1 1) (1 2) |
+    ** | -1 0 1 |   | (2 0) (2 0) (2 2) |
+    **/
 
+    // Copy back the greyed pixel
+    __syncthreads();
+    // dr[idx] = value;
+    // dg[idx] = value;
+    // db[idx] = value;
 }
 
 int main(int argc, char** argv){
@@ -79,11 +96,6 @@ int main(int argc, char** argv){
     ui32* img = (ui32*)malloc(sizeof(ui32) * 3 * IMG_SIZE);
     if(img == NULL){
         perror("Memory allocation for img array failed.\n");
-        exit(1);
-    }
-    ui32* h_img = (ui32*)malloc(3 * IMG_SIZE);
-    if(h_img == NULL){
-        perror("Memory allocation for temporary array failed.\n");
         exit(1);
     }
 
@@ -135,31 +147,31 @@ int main(int argc, char** argv){
     // 1024 threads per blocs in 2D
     dim3 Threads_Per_Blocks(32, 32);
     // Blocks for inverted image
-    // dim3 Num_Blocks(3 * width/Threads_Per_Blocks.x + 1, height/Threads_Per_Blocks.y + 1);
+    dim3 Num_Blocks(3 * width/Threads_Per_Blocks.x, height/Threads_Per_Blocks.y);
     // Blocks for gray scale
-    dim3 Num_Blocks(width/Threads_Per_Blocks.x + 1, height/Threads_Per_Blocks.y + 1);
+    // dim3 Num_Blocks(width/Threads_Per_Blocks.x + 1, height/Threads_Per_Blocks.y + 1);
 
     // Copy to device
     cudaMemcpy(d_img, img, 3 * IMG_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(dr, hr, IMG_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(dg, hg, IMG_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(db, hb, IMG_SIZE, cudaMemcpyHostToDevice);
+    // cudaMemcpy(dr, hr, IMG_SIZE, cudaMemcpyHostToDevice);
+    // cudaMemcpy(dg, hg, IMG_SIZE, cudaMemcpyHostToDevice);
+    // cudaMemcpy(db, hb, IMG_SIZE, cudaMemcpyHostToDevice);
 
     //Horizontal Symetry
-    grey_img<<<Num_Blocks, Threads_Per_Blocks>>>(dr, dg, db, width, height);
+    horizontal_sym<<<Num_Blocks, Threads_Per_Blocks>>>(d_img, d_tmp, width, height);
     // Copy to Host
-    // cudaMemcpy(img, d_tmp, 3 * IMG_SIZE, cudaMemcpyDeviceToHost);
-    cudaMemcpy(hr, dr, IMG_SIZE, cudaMemcpyDeviceToHost);
-    cudaMemcpy(hg, dg, IMG_SIZE, cudaMemcpyDeviceToHost);
-    cudaMemcpy(hb, db, IMG_SIZE, cudaMemcpyDeviceToHost);
-    for(ui32 y = 0U; y < height; ++y){
-        for(ui32 x = 0U; x < 3 * width; ++x){
-            int idx = ((y * width) + x) * 3;
-            img[idx + 0] = hr[y * width + x];
-            img[idx + 1] = hg[y * width + x];
-            img[idx + 2] = hb[y * width + x];
-        }
-    }
+    cudaMemcpy(img, d_tmp, 3 * IMG_SIZE, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(hr, dr, IMG_SIZE, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(hg, dg, IMG_SIZE, cudaMemcpyDeviceToHost);
+    // cudaMemcpy(hb, db, IMG_SIZE, cudaMemcpyDeviceToHost);
+    // for(ui32 y = 0U; y < height; ++y){
+    //     for(ui32 x = 0U; x < 3 * width; ++x){
+    //         int idx = ((y * width) + x) * 3;
+    //         img[idx + 0] = hr[y * width + x];
+    //         img[idx + 1] = hg[y * width + x];
+    //         img[idx + 2] = hb[y * width + x];
+    //     }
+    // }
 
     bits = (BYTE*)FreeImage_GetBits(bitmap);
     for(int y =0; y<height; y++)
@@ -186,6 +198,5 @@ int main(int argc, char** argv){
     FreeImage_DeInitialise(); //Cleanup !
 
     free(img);
-    free(h_img);
     return 0;
 }
